2b2a37c9340b0a181dd351b35e882071191100f2
Balthair94 <archerbaltazar@gmail.com>
2017-10-18 23:49:00 -0500

    Kotlin added and styles

--- app/src/main/java/baltamon/mx/preguntame/MainActivity.kt
+++ app/src/main/java/baltamon/mx/preguntame/MainActivity.kt
@@ -1,12 +1,25 @@
 package baltamon.mx.preguntame
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupToolbar()
+    }
+
+    fun setupToolbar(){
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.main_title)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
